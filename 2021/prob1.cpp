64c3459a8131bc195f866bf618f0e421a1bb21a5
AureliaSindhu <aurelia.sindhu@gmail.com>
2025-06-27 10:41:32 -0700

    prob1_2021 solved

--- 2021/prob1.cpp
+++ 2021/prob1.cpp
@@ -1,8 +1,8 @@
 /* 
  * File:   main.cpp
  * Author: Aurelia Sindhunirmala
- * Created on February 14, 2023, 10:22 AM
- * Purpose: To be used as a template for all code in CIS/CSC 5 Spring 2023
+ * Created on June 27, 2025
+ * Purpose: Problem 1 ICPC 2021
  */
 
 // System Libraries
@@ -20,10 +20,23 @@ int main(int argc, char** argv) {
     //Set random number seed
     
     //Declare variables
+    const int radius[] = {1433, 1433, 2000, 600};
+    const float sElv[] = {.09, .12, .09, .12};
+
+    int v, r;
+    float s, part1, part2; 
     
     //Initialize Inputs
     
     //Map Inputs to Outputs - Process
+    for (int i = 0; i < 4; i++) {
+        r = radius[i];
+        s = sElv[i];
+        part1 = r * (s + .16);
+        part2 = part1 / 0.067;
+        v = sqrt(part2);
+        cout << "v" << i << " = " << v << endl;
+    }
     
     //Display output
     
